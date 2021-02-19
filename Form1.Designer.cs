ff2a53a7ec3ad12dcf39f645684bc802cb479a1d
forsenboylul <77509467+forsenboylul@users.noreply.github.com>
2021-02-19 18:10:21 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace ooplab41
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowB = new System.Windows.Forms.Button();
+            this.DeleteCB = new System.Windows.Forms.Button();
+            this.ClearSB = new System.Windows.Forms.Button();
+            this.ClearB = new System.Windows.Forms.Button();
+            this.PanelD = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ShowB
+            // 
+            this.ShowB.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ShowB.Location = new System.Drawing.Point(9, 30);
+            this.ShowB.Margin = new System.Windows.Forms.Padding(4);
+            this.ShowB.Name = "ShowB";
+            this.ShowB.Size = new System.Drawing.Size(281, 92);
+            this.ShowB.TabIndex = 0;
+            this.ShowB.Text = "Отобразить все объекты";
+            this.ShowB.UseVisualStyleBackColor = false;
+            this.ShowB.Click += new System.EventHandler(this.ShowB_Click);
+            // 
+            // DeleteCB
+            // 
+            this.DeleteCB.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.DeleteCB.Location = new System.Drawing.Point(9, 330);
+            this.DeleteCB.Margin = new System.Windows.Forms.Padding(4);
+            this.DeleteCB.Name = "DeleteCB";
+            this.DeleteCB.Size = new System.Drawing.Size(281, 92);
+            this.DeleteCB.TabIndex = 1;
+            this.DeleteCB.Text = "Удалить выделенные элементы";
+            this.DeleteCB.UseVisualStyleBackColor = false;
+            this.DeleteCB.Click += new System.EventHandler(this.DeleteCB_Click);
+            // 
+            // ClearSB
+            // 
+            this.ClearSB.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClearSB.Location = new System.Drawing.Point(9, 230);
+            this.ClearSB.Margin = new System.Windows.Forms.Padding(4);
+            this.ClearSB.Name = "ClearSB";
+            this.ClearSB.Size = new System.Drawing.Size(281, 92);
+            this.ClearSB.TabIndex = 2;
+            this.ClearSB.Text = "Очистить хранилище";
+            this.ClearSB.UseVisualStyleBackColor = false;
+            this.ClearSB.Click += new System.EventHandler(this.ClearSelected_Click);
+            // 
+            // ClearB
+            // 
+            this.ClearB.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClearB.Location = new System.Drawing.Point(9, 130);
+            this.ClearB.Margin = new System.Windows.Forms.Padding(4);
+            this.ClearB.Name = "ClearB";
+            this.ClearB.Size = new System.Drawing.Size(281, 92);
+            this.ClearB.TabIndex = 3;
+            this.ClearB.Text = "Скрыть объекты на панели";
+            this.ClearB.UseVisualStyleBackColor = false;
+            this.ClearB.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // PanelD
+            // 
+            this.PanelD.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.PanelD.Location = new System.Drawing.Point(298, 30);
+            this.PanelD.Margin = new System.Windows.Forms.Padding(4);
+            this.PanelD.Name = "PanelD";
+            this.PanelD.Size = new System.Drawing.Size(756, 507);
+            this.PanelD.TabIndex = 4;
+            this.PanelD.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PanelD_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Полотно";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PanelD);
+            this.Controls.Add(this.ClearB);
+            this.Controls.Add(this.ClearSB);
+            this.Controls.Add(this.DeleteCB);
+            this.Controls.Add(this.ShowB);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ShowB;
+        private System.Windows.Forms.Button DeleteCB;
+        private System.Windows.Forms.Button ClearSB;
+        private System.Windows.Forms.Button ClearB;
+        private System.Windows.Forms.Panel PanelD;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ooplab41
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Кол-во ячеек в хранилище
+        static int av = 5;
+
+        // Создаем хранилище
+        Container container = new Container(av);
+
+        // Счетчик отображаемых объектов
+        static int index = 0;
+
+        // Индекс элемента хранилища
+        int indexC = 0;
+
+        // Идентификатор нажатия Control
+        int ctrlPressed = 0;
+
+        class Circle
+        {
+            public int x, y;
+            public int r = 20;
+            public Color color = Color.Maroon;
+            public bool visible = true;
+
+            public Circle()
+            {
+                x = 0;
+                y = 0;
+            }
+
+            public Circle(int x, int y)
+            {
+                this.x = x - r;
+                this.y = y - r;
+            }
+
+            ~Circle() { }
+
+        }
+
+        class Container {
+            public Circle[] m_data;
+
+            public Container(int count)
+            {
+                m_data = new Circle[count];
+                for (int i = 0; i < count; ++i)
+                {
+                    m_data[i] = null;
+                }
+            }
+
+            public void place(int count)
+            {
+                m_data = new Circle[count];
+                for (int i = 0; i < count; ++i)
+                {
+                    m_data[i] = null;
+                }
+            }
+
+            public void add_object(int ind, ref Circle object1, int count, ref int indexC)
+            {
+                while (m_data[ind] != null)
+                {
+                    ind = (ind + 1) % count;
+                }
+                m_data[ind] = object1;
+                indexC = ind;
+            }
+
+            public void deleter(int ind)
+            {
+                m_data[ind] = null;
+                index--;
+            }
+
+            public bool emptynessCheck(int index)
+            {
+                if (m_data[index] == null)
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            public int oc(int size)
+            {
+                int count_oc = 0;
+                for (int i = 0; i < size; ++i)
+                {
+                    if (!emptynessCheck(i))
+                    {
+                        ++count_oc;
+                    }
+                }
+                return count_oc;
+            }
+
+            public void Dsize(ref int size)
+            {
+                Container storage1 = new Container(size * 2);
+                for (int i = 0; i < size; ++i)
+                {
+                    storage1.m_data[i] = m_data[i];
+                }
+                for (int i = size; i < (size * 2) - 1; ++i)
+                {
+                    storage1.m_data[i] = null;
+                }
+                size = size * 2;
+                place(size);
+                for (int i = 0; i < size; i++)
+                {
+                    m_data[i] = storage1.m_data[i];
+                }
+            }
+
+            ~Container() { }
+        }
+
+
+        private void paint_circle(Color name, ref Container stg, int index)
+        {
+            Pen pen = new Pen(name, 3);
+            if (!container.emptynessCheck(index))
+            {
+                if (container.m_data[index].visible == true)
+                {
+                    PanelD.CreateGraphics().DrawEllipse(
+                    pen,
+                    stg.m_data[index].x,
+                    stg.m_data[index].y,
+                    stg.m_data[index].r * 2,
+                    stg.m_data[index].r * 2);
+                    stg.m_data[index].color = name;
+                }
+            }
+        }
+        private void remove_selection_circle(ref Container stg)
+        {   // Снимает выделение у всех элементов хранилища
+            for (int i = 0; i < av; ++i)
+            {
+                if (!container.emptynessCheck(i))
+                {   // Вызываем функцию отрисовки круга
+                    paint_circle(Color.Maroon, ref container, i);
+                }
+            }
+        }
+
+        private void remove_selected_circle(ref Container stg)
+        {   // Удаляет выделенные элементы из хранилища
+            for (int i = 0; i < av; ++i)
+            {
+                if (!container.emptynessCheck(i))
+                {
+                    if (container.m_data[i].color == Color.LimeGreen)
+                    {
+                        container.deleter(i);
+                    }
+                }
+            }
+        }
+
+        private int check_circle(ref Container stg, int size, int x, int y)
+        {   // Проверяет есть ли уже круг с такими же координатами в хранилище
+            if (stg.oc(size) != 0)
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    if (!stg.emptynessCheck(i))
+                    {
+                        int x1 = stg.m_data[i].x - 15;
+                        int x2 = stg.m_data[i].x + 15;
+                        int y1 = stg.m_data[i].y - 15;
+                        int y2 = stg.m_data[i].y + 15;
+
+                        // Если круг есть, возвращет индекс круга в хранилище
+                        if ((x1 <= x && x <= x2) && (y1 <= y && y <= y2))
+                            return i;
+                    }
+                }
+            }
+            return -1;
+        }
+
+        private void PanelD_MouseClick(object sender, MouseEventArgs e)
+        {
+            Circle circle1 = new Circle(e.X, e.Y);
+            if (index == av)
+            {
+                container.Dsize(ref av);
+            }
+            int c = check_circle(ref container, av, circle1.x, circle1.y);
+            if (c != -1)
+            {
+                if (Control.ModifierKeys == Keys.Control)
+                {
+                    if (ctrlPressed == 0)
+                    {
+                        paint_circle(Color.Maroon, ref container, indexC);
+                        ctrlPressed = 1;
+                    }
+                    paint_circle(Color.LimeGreen, ref container, c);
+                }
+                else
+                {
+                    remove_selection_circle(ref container);
+                    paint_circle(Color.LimeGreen, ref container, c);
+                }
+                return;
+            }
+            container.add_object(index, ref circle1, av, ref indexC);
+            remove_selection_circle(ref container);
+            paint_circle(Color.LimeGreen, ref container, indexC);
+            ++index;
+            ctrlPressed = 0;
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            PanelD.Refresh();
+            for (int i = 0; i < av; ++i)
+            {
+                if (!container.emptynessCheck(i))
+                {
+                    container.m_data[i].visible = false;
+                }
+            }
+        }
+
+        private void ClearSelected_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < av; ++i)
+            {
+                container.m_data[i] = null;
+            }
+            index = 0;
+            PanelD.Refresh();
+        }
+
+        private void DeleteCB_Click(object sender, EventArgs e)
+        {
+            remove_selected_circle(ref container);
+            PanelD.Refresh();
+            if (container.oc(av) != 0)
+            {
+                for (int i = 0; i < av; ++i)
+                {
+                    paint_circle(Color.Maroon, ref container, i);
+                }
+            }
+        }
+
+        private void ShowB_Click(object sender, EventArgs e)
+        {
+            PanelD.Refresh();
+            if (container.oc(av) != 0)
+            {
+                for (int i = 0; i < av; ++i)
+                {
+                    if (!container.emptynessCheck(i))
+                    {
+                        container.m_data[i].visible = true;
+                    }
+                    paint_circle(Color.Maroon, ref container, i);
+                }
+            }
+        }
+    }
+}
